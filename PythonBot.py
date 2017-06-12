88ede45a1c2a51803683284ddf832225b44effcb
KuCharYen <gapdevil33@hotmail.com>
2017-06-12 21:34:00 +0700

    Create PythonBot.py

--- PythonBot.py
+++ PythonBot.py
@@ -1,73 +1,48 @@
 from flask import Flask, request
-
-import base64
-import hashlib
-import hmac
-
 import json
-#import requests
-
+import requests
+ 
 app = Flask(__name__)
-
+ 
 access_token = 'qgdntcUqQibpcFZwTLOarjqCmPtXS/d5E8zovhoZS/Rd7e1Ue0JLVMzSGLwQB6r4+rDpddQcEBRILXlWXA4X80ZNWRToRf1ex1oi2RqR/jXjb/iNQbHjlZmxnqLfgKDnnlSa1KWeGtWqxptHoD0dGgdB04t89/1O/w1cDnyilFU='
 
-# Load Json file to test
-#with open('Webhook.json') as json_file:
-##with open('JSonBody.json') as json_file:
-##    events = json.load(json_file)
-    #pprint(events)
     
-@app.route("/callback", methods=['POST'])
+@app.route('/')
+def index():
+    return "Hello World!"
+# ส่วน callback สำหรับ Webhook
+@app.route('/callback', methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    events = request.get_data(as_text=True)
-    app.logger.info("Request body: " + events)
-
-#events = requests.get(as_text=True)
-
-
-# Validate parsed JSon data
-if(events != None):
-    # Loop through each event
-    for event in body_json['events']:
-        # Reply only text message.
-        if(event['type'] == 'message' and event['message']['type'] == 'text'):
-            # Get message and replyToken
-            userID = event['source']['userId']
-            replyToken = event['replyToken']
-            text = event['message']['text']
-            
-            # User message content
-            #print('\nUser Said')
-            #print('UserID: ' + userID)
-            #print('ReplyToken: ' + replyToken )
-            #print('Text: ' + text)
-
-            # Build message to reply back.
-            replyMessage = {'type' : 'text', 'text' : text}
-
-            # Bot reply message content
-            #print('\nLine Bot Reply')
-            #print(replyMessage['text'])
-
-            # Make Post Request to Messaging API to reply to sender.
-            url = 'https://api.line.me/v2/bot/message/reply'
-            data = {'replyToken' : replyToken, 'messages' : replyMessage}
-    
-            # Parse reply message to Json 
-            post = json.JSONEncoder().encode(data)            
-            #print(post)
-            
-            headers = ['Content-Type: application/json', 'Authorization: Bearer ' + access_token]
-            
-            requests.post(url,post)
-            
-            #print(requests)
-            
-if __name__ == "__main__":
-    app.run()
-    
-echo 'OK';                    
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded["events"][0]['replyToken']
+    #id=[d['replyToken'] for d in user][0]
+    #print(json_line)
+    print("ผู้ใช้：",user)
+    sendText(user,'งง') # ส่งข้อความ งง
+    return '',200
+ 
+def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = 'Bearer '+ access_token # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+ 
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+ 
+    data = json.dumps({
+        "replyToken":user,
+        "messages":[{
+            "type":"text",
+            "text":text
+        }]
+    })
+ 
+    #print("ข้อมูล：",data)
+    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+    #print(r.text)
+ 
+if __name__ == '__main__':
+     app.run(debug=True)
